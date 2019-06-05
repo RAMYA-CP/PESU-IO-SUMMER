74bb5b0299580f10b9dc8852eed651b05e040c03
RAMYA-CP <48626414+RAMYA-CP@users.noreply.github.com>
2019-06-05 22:31:19 +0530

    assignment

---
+++ coding_assignment_module1/five.py
@@ -0,0 +1,6 @@
+#Write a Python program to check if a string is numeric.
+p=input("ENTER THE STRING:")#input returns a string
+if(p.isdigit()):
+    print("THE GIVEN STRING IS A NUMERIC")
+else:
+    print("THE GIVEN STRING IS NOT A NUMERIC")
